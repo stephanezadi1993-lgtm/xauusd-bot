9f2eb9c88deb9749df8659d0df67c56a6add5b55
stephanezadi1993-lgtm <stephanezadi1993@gmail.com>
2026-06-18 09:03:55 +0000

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -1,7 +1,16 @@
+import asyncio
 import subprocess
 import sys
+import threading
 
-p1 = subprocess.Popen([sys.executable, "bot.py"])
-p2 = subprocess.Popen([sys.executable, "bot_synthetic.py"])
-p1.wait()
-p2.wait()
+def run_bot(script):
+    subprocess.run([sys.executable, script])
+
+t1 = threading.Thread(target=run_bot, args=("bot.py",))
+t2 = threading.Thread(target=run_bot, args=("bot_synthetic.py",))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
